52672640edb77e5d1239e5e463ba4834c3c7d4f5
airtonfel <airtons.f@hotmail.com>
2021-05-08 01:36:37 -0300

    Adicionar arquivos de projeto.

---
+++ ExceptionHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExceptionHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int idade;
+                Console.WriteLine("Digite a sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("A sua idade é: " + idade);
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine("Erro: O valor informado não é numérico. " + erro);
+                throw;
+            }
+        }
+    }
+}
